816b7520cec94de98ef168ff69f1f2389c0e652a
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2023-08-17 13:57:47 -0300

    Ensure archives contain only regular files

--- src/unarchiver.rs
+++ src/unarchiver.rs
@@ -72,11 +72,20 @@ impl Unarchiver {
 
         for maybe_entry in archive.entries()? {
             let mut entry = maybe_entry?;
+            let header = entry.header();
+            let entry_size = header.entry_size().unwrap_or(12500);
 
-            let path = entry.path()?.to_path_buf();
+            match header.entry_type() {
+                EntryType::Regular => {},
+                other => {
+                    eprintln!("decompressing: Found a {:?} file, expected Regular. Ignoring", other);
+                    continue;
+                },
+            }
+
+            let path = entry.path()?.into();
 
             let contents = {
-                let entry_size = entry.header().entry_size().unwrap_or(12500);
                 let mut buf = Vec::with_capacity(entry_size as usize);
 
                 entry.read_to_end(&mut buf)?;
@@ -87,82 +96,5 @@ impl Unarchiver {
         }
 
         Ok(Archive { entries })
-    }
-
-    #[allow(unused)]
-    async fn ensure_has_shared_objects(file_name: &str) -> Result<()> {
-        let arguments = ["-tf", file_name, "--wildcards", "*.so"];
-
-        let output = Command::new("tar")
-            .args(arguments)
-            .stdout(Stdio::null())
-            .stderr(Stdio::null())
-            .spawn()?
-            .wait()
-            .await?;
-
-        anyhow::ensure!(
-            output.success(),
-            "{file_name} did not contain a shared object"
-        );
-        Ok(())
-    }
-
-    #[allow(unused)]
-    pub async fn extract_shared_objs(path: &Path, save_to: &Path) -> Result<String> {
-        let file_name = path_to_str(path)?;
-        let export_to = path_to_str(save_to)?;
-
-        Self::ensure_has_shared_objects(file_name).await?;
-
-        let arguments = ["-xzvf", file_name, "-C", export_to, "--wildcards", "*.so"];
-
-        // Take from the archive only .so files
-        let output = Command::new("tar")
-            .args(arguments)
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .spawn()?
-            .wait_with_output()
-            .await?;
-
-        if output.status.success() {
-            // In stdout we'll find the files that were decompressed
-            let utf8_output = String::from_utf8(output.stdout)?;
-
-            Ok(utf8_output)
-        } else {
-            let utf8_output = String::from_utf8(output.stderr)?;
-
-            anyhow::bail!("Failed to decompress archive: {utf8_output}");
-        }
-    }
-
-    #[allow(unused)]
-    pub async fn extract_all(path: &Path, save_to: &Path) -> Result<String> {
-        let file_name = path_to_str(path)?;
-        let export_to = path_to_str(save_to)?;
-
-        let arguments = ["-xzvf", file_name, "-C", export_to];
-
-        // Take from the archive only .so files
-        let output = Command::new("tar")
-            .args(arguments)
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .spawn()?
-            .wait_with_output()
-            .await?;
-
-        if output.status.success() {
-            // In stdout we'll find the files that were decompressed
-            let utf8_output = String::from_utf8(output.stdout)?;
-
-            Ok(utf8_output)
-        } else {
-            let utf8_output = String::from_utf8(output.stderr)?;
-
-            anyhow::bail!("Failed to decompress archive: {utf8_output}");
-        }
     }
 }
